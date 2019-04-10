24ba7e3a0365675a7dd46fb03346d07b77ef7842
lujun <506991096@qq.com>
2019-04-10 20:20:04 +0800

    remote file download

---
+++ microservice-remotefile/src/main/java/org/corgi/remotefile/RemotefileApplication.java
@@ -0,0 +1,13 @@
+package org.corgi.remotefile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemotefileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemotefileApplication.class, args);
+    }
+
+}
---
+++ microservice-remotefile/src/test/java/org/corgi/remotefile/MicroserviceRemotefileApplicationTests.java
@@ -0,0 +1,16 @@
+package org.corgi.remotefile;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroserviceRemotefileApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
